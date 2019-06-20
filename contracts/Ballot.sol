7f283c22ada809bf611fb74b3e95a14523eb88ab
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-06-20 18:05:45 +0200

    Static proposal adding done.

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,9 +2,24 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Ballot {
 
-  string public proposal;
+  struct Proposal {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // Fetch Proposals
+  mapping(uint => Proposal) public proposals;
+  // Store Proposals count
+  uint public proposalsCount;
 
   constructor() public {
-    proposal = "Proposal 1";
+    addProposal("Proposal 1");
+    addProposal("Proposal 2");
+  }
+
+  function addProposal (string memory _proposal) private {
+    proposalsCount ++;
+    proposals[proposalsCount] = Proposal(proposalsCount, _proposal, 0);
   }
 }
