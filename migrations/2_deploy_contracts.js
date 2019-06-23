6bfa3be30bbcb422ee1f41d846c7f45d0321a18a
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-06-23 17:37:30 +0200

    Migration file with static init data.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
 const Ballot = artifacts.require("Ballot");
+const crypto = require("crypto");
 
 module.exports = deployer => {
-  deployer.deploy(Ballot);
+  const serverHash = crypto.createHash("sha256");
+  serverHash.update("SomeServerNameHash");
+
+  const settingsHash = crypto.createHash("sha256");
+  settingsHash.update("SomeSettingsHash");
+
+  deployer.deploy(
+    Ballot,
+    `0x${serverHash.digest("hex")}`,
+    `0x${settingsHash.digest("hex")}`
+  );
 };
